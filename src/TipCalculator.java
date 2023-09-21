c30614f367d328ca0b61a5e8f6d2c1d83b2b81c5
aivin1 <aivinni1219@gmail.com>
2023-09-21 10:56:47 -0400

    content

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -6,6 +6,6 @@ public class TipCalculator {
         System.out.println("Welcome to Tip Diner!");
         System.out.print("What is your name?");
         String name = scan.nextLine();
-
+        System.out.print("Hello, " + name + " how many people are in your group?");
     }
 }
